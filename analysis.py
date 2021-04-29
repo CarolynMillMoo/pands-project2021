cc895211229a19a32ab7daa4ff74af2eadcec461
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-29 22:24:50 +0100

    Using a tutorial to learn more about data interpretation-trials

--- analysis.py
+++ analysis.py
@@ -2,7 +2,22 @@
 #Iris Dataset
 #Author: Carolyn Moorhouse
 
-import pandas as pandas
-data = pd.read_csv("iris.iris.csv")
+import pandas as pd
+data = pd.read_csv("iris.csv")
 
-#
+#the function head() will display the top rows of the dataset "iris.csv"
+#the default value of this function is 5
+#so it will show the top 5 rows when no argument is given
+
+data.head()
+print(data.head())
+
+data.sample(10)
+
+data.columns
+print(data.columns)
+
+data.shape
+print(data.shape)
+
+print(data)
