dff3e72eb145cf1c07362dc7cddc043a39b8853f
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-30 17:36:57 +0100

    Added in the total, mean and median sepal width for all species this information is also added to iris txt file

--- analysis.py
+++ analysis.py
@@ -16,6 +16,8 @@ print(data["species"].value_counts())
 with open("iris.txt", "w") as f:
     data = f.write("Instance of each species of iris: "+str(data["species"].value_counts()))
 
+#The following calculates the sum, mean and median of the sepal length for all species
+#and also appends this info to the iris txt file
 data = pd.read_csv("iris.csv")
 
 sum_sepallength = data["sepallengthcm"].sum()
@@ -28,6 +30,38 @@ with open("iris.txt", "a") as f:
     data = f.write('\nTotal Sepal Length all species Sum(cm):'+str(sum_sepallength))
     data = f.write('\nMean Sepal length all species (cm):'+str(mean_sepallength))
     data = f.write('\nMean Sepal length all species (cm): '+str(median_sepallength))
+
+#the following calculate the sum, mean and median of the sepal width for all species
+#and also appends this to the iris txt file
+data = pd.read_csv("iris.csv")
+
+sum_sepalwidth = data["sepalwidthcm"].sum()
+mean_sepalwidth = data["sepalwidthcm"].mean()
+median_sepalwidth = data["sepalwidthcm"].median()
+
+print("Sepal Width Sum(cm):",sum_sepalwidth, "\nMean Sepal Width(cm):", mean_sepalwidth, "\nMedian Sepal width(cm):", median_sepalwidth)
+
+with open("iris.txt", "a") as f:
+    data = f.write('\nTotal Sepal width all species Sum(cm):'+str(sum_sepalwidth))
+    data = f.write('\nMean Sepal width all species (cm):'+str(mean_sepalwidth))
+    data = f.write('\nMean Sepal width all species (cm): '+str(median_sepalwidth))
+
+#the following calculate the sum, mean and median of the petal length for all species
+#and also appends this to the iris txt file
+data = pd.read_csv("iris.csv")
+
+sum_petallength = data["petallengthcm"].sum()
+mean_petallength = data["petallength"].mean()
+median_petallength = data["sepalwidthcm"].median()
+
+print("Sepal Width Sum(cm):",sum_sepalwidth, "\nMean Sepal Width(cm):", mean_sepalwidth, "\nMedian Sepal width(cm):", median_sepalwidth)
+
+with open("iris.txt", "a") as f:
+    data = f.write('\nTotal Sepal width all species Sum(cm):'+str(sum_sepalwidth))
+    data = f.write('\nMean Sepal width all species (cm):'+str(mean_sepalwidth))
+    data = f.write('\nMean Sepal width all species (cm): '+str(median_sepalwidth))
+
+
 
 
 
