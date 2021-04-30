ae88e5b53dc0081927bc500bb814c42ce4fe2490
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-30 13:37:22 +0100

    began sum, median, mean calculations and print to file- had some errors to work out so only have a basic Sepal Length calculation complete- need to seperate into the three species

--- analysis.py
+++ analysis.py
@@ -3,11 +3,28 @@
 #Author: Carolyn Moorhouse
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
 data = pd.read_csv("iris.csv")
 
-numericaldata = data[["sepallengthcm", "sepalwidthcm", "petallengthcm", "petalwidthcm"]]
-#print(numericaldata)
-data.iloc[5]
+#Species count: counting the instance of each species
+#in the iris.txt file
+data["species"].value_counts()
+print(data["species"].value_counts())
 
-data.loc[data["species"] == "Iris-setosa"]
-print(data.loc)
+sum_sepallength = data["sepallengthcm"].sum()
+mean_sepallength = data["sepallengthcm"].mean()
+median_sepallength = data["sepallengthcm"].median()
+
+with open("iris.txt", "w") as f:
+    data = f.write('Sepal Length Sum(cm):'+str(sum_sepallength))
+    print(data)
+
+print("Sepal Length Sum(cm):",sum_sepallength, "\nMean Sepal Length(cm):", mean_sepallength, "\nMedian Sepal Length(cm):", median_sepallength)
+
+
+
+
+
