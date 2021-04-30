19e42946b6b0ebba3576410a64fff382b6cfe6de
Carolyn Moorhouse <G00398299@gmit.ie>
2021-04-30 16:59:58 +0100

    Summary of instance of species added to txt file

--- analysis.py
+++ analysis.py
@@ -13,14 +13,17 @@ data = pd.read_csv("iris.csv")
 #in the iris.txt file
 data["species"].value_counts()
 print(data["species"].value_counts())
-
-sum_sepallength = data["sepallengthcm"].sum()
-mean_sepallength = data["sepallengthcm"].mean()
-median_sepallength = data["sepallengthcm"].median()
-
 with open("iris.txt", "w") as f:
-    data = f.write('Sepal Length Sum(cm):'+str(sum_sepallength))
-    print(data)
+    data = f.write("Instance of each species of iris: "+str(data["species"].value_counts()))
+
+#sliced_data = data[2:51]
+#sum_sepallength = data["sepallengthcm"].sum(sliced_data)
+#mean_sepallength = data["sepallengthcm"].mean()
+#median_sepallength = data["sepallengthcm"].median()
+
+#with open("iris.txt", "w") as f:
+ #   data = f.write('Sepal Length Sum(cm):'+str(sum_sepallength))
+  #  print(data)
 
 print("Sepal Length Sum(cm):",sum_sepallength, "\nMean Sepal Length(cm):", mean_sepallength, "\nMedian Sepal Length(cm):", median_sepallength)
 
